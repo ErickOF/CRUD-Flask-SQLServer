45b07f529ee6080e1a8f7434286bd3c392f01b9c
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-05-06 09:27:21 -0600

    [Feature] - CRUD

--- API/main.py
+++ API/main.py
@@ -13,6 +13,107 @@ CORS(app)
 connection = pymssql.connect(config['host'], config['user'],
                              config['password'], config['db'])
 
+
+@app.route('/users/create', methods=['POST'])
+def createUser():
+    userInfo = [request.json['idCard'], request.json['name'],
+            request.json['lastname'], request.json['phoneNumber'],
+            request.json['email']]
+    
+    cursor = connection.cursor()
+
+    response = {
+        'message': 'Unkown error.',
+        'status': 404,
+        'data': None
+    }
+
+    try:
+        cursor.callproc('createUser', userInfo)
+        connection.commit()
+
+        response = {
+            'message': 'User was created.',
+            'status': 400,
+            'data': None
+        }
+    except pymssql.OperationalError as e:
+        print(e)
+    except pymssql.DatabaseError:
+        response = {
+            'message': 'ID Card already exists.',
+            'status': 400,
+            'data': None
+        }
+
+    cursor.close()
+
+    return jsonify(response)
+
+@app.route('/users/<_id>', methods=['DELETE'])
+def deleteUser(_id):
+    cursor = connection.cursor()
+    cursor.callproc('deleteUser', [_id])
+    cursor.nextset()
+
+    response = {}
+
+    try:
+        connection.commit()
+
+        response = {
+            'message': 'User was deleted.',
+            'status': 400,
+            'data': None
+        }
+    except pymssql.OperationalError:
+        response = {
+            'message': 'User not found.',
+            'status': 404,
+            'data': None
+        }
+
+    cursor.close()
+
+    return jsonify(response)
+
+@app.route('/users/<_id>', methods=['GET'])
+def getUserById(_id):
+    cursor = connection.cursor()
+    cursor.callproc('getUserByID', [_id])
+    cursor.nextset()
+
+    response = {}
+
+    try:
+        result = cursor.fetchone()
+        connection.commit()
+        
+        user = {
+            '_id': result[1],
+            'idCard': result[0],
+            'email': result[5],
+            'lastname': result[3],
+            'name': result[3],
+            'phoneNumber': result[4]
+        }
+
+        response = {
+            'message': 'Returing user.',
+            'status': 400,
+            'data': user
+        }
+    except pymssql.OperationalError:
+        response = {
+            'message': 'User not found.',
+            'status': 404,
+            'data': None
+        }
+
+    cursor.close()
+
+    return jsonify(response)
+
 @app.route('/users', methods=['GET'])
 def getUsers():
     cursor = connection.cursor()
@@ -38,13 +139,47 @@ def getUsers():
         pass
 
     cursor.close()
-    connection.close()
 
     return jsonify({
         'message': 'Returing all users.',
         'status': 400,
         'data': users
     })
+
+@app.route('/users/<_id>', methods=['PUT'])
+def updateUser(_id):
+    userInfo = [_id, request.json['idCard'], request.json['name'],
+            request.json['lastname'], request.json['phoneNumber'],
+            request.json['email']]
+    
+    cursor = connection.cursor()
+
+    response = {
+        'message': 'Unkown error.',
+        'status': 404,
+        'data': None
+    }
+
+    try:
+        cursor.callproc('updateUser', userInfo)
+        connection.commit()
+
+        response = {
+            'message': 'User was updated.',
+            'status': 400,
+            'data': None
+        }
+    except pymssql.OperationalError as e:
+        response = {
+            'message': 'ID Card not found.',
+            'status': 400,
+            'data': None
+        }
+
+    cursor.close()
+
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
