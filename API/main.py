c77ea377c52541c04595141a7e0bcfc100ab7fb5
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-05-05 20:01:41 -0600

    [Feature] - Get Users

--- API/main.py
+++ API/main.py
@@ -13,7 +13,38 @@ CORS(app)
 connection = pymssql.connect(config['host'], config['user'],
                              config['password'], config['db'])
 
+@app.route('/users', methods=['GET'])
+def getUsers():
+    cursor = connection.cursor()
+    cursor.callproc('getUsers')
+    cursor.nextset()
+    users = []
 
+    try:
+        results = cursor.fetchall()
+        connection.commit()
+        
+        users = []
+        for user in results:
+            users.append({
+                '_id': user[1],
+                'idCard': user[0],
+                'email': user[5],
+                'lastname': user[3],
+                'name': user[3],
+                'phoneNumber': user[4]
+            })
+    except pymssql.OperationalError:
+        pass
+
+    cursor.close()
+    connection.close()
+
+    return jsonify({
+        'message': 'Returing all users.',
+        'status': 400,
+        'data': users
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
